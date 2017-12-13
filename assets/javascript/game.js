3e9f8e88ed9c7158b2e1d93100358cbf2cc4d78e
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2017-12-12 19:05:20 -0500

    functionality all set

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+
+var userScore = 0;
+var winEntry = 0;
+var lossEntry = 0;
+
+var targetScore = 19 + Math.floor(Math.random() * 120);
+$("#target-score").text(targetScore);
+
+var counter = 1 + Math.floor(Math.random() * 12);
+$(".crystal-image").on("click", function() {
+	counter += 1 + Math.floor(Math.random() * 12);
+	document.querySelector("#user-score").innerHTML = counter + userScore;
+	if (counter === targetScore) {
+		alert("You win!");
+		document.querySelector("#win-entries").innerHTML = ++winEntry;
+		reset();
+	} else if (counter >= targetScore) {
+		alert("You lose!!");
+		document.querySelector("#loss-entries").innerHTML = ++lossEntry;
+		reset();
+	}
+	if (winEntry === 1) {
+		alert("You've unlocked an item!");
+		$(".crystal-image-unlock1").attr(
+			"src",
+			"http://via.placeholder.com/200x200/000000/ffffff"
+		);
+	}
+});
+
+function reset() {
+	targetScore = 19 + Math.floor(Math.random() * 120);
+	$("#user-score").text(userScore);
+	$("#target-score").text(targetScore);
+	console.log(targetScore);
+	userScore = 0;
+	counter = 0;
+
+	console.log(userScore);
+	$(".crystal-image").on("click", function() {
+		counter += 1 + Math.floor(Math.random() * 12);
+		document.querySelector("#user-score").innerHTML = counter + userScore;
+		if (counter === targetScore) {
+			alert("You win! Item unlocked.");
+			document.querySelector("#win-entries").innerHTML = ++winEntry;
+			reset();
+		} else if (counter >= targetScore) {
+			alert("You lose!!");
+			document.querySelector("#loss-entries").innerHTML = ++lossEntry;
+			reset();
+		}
+		if (winEntry === 1) {
+			$(".crystal-image-unlock").attr(
+				"src",
+				"http://via.placeholder.com/200x200/000000/ffffff"
+			);
+			reset();
+		}
+	});
+}
