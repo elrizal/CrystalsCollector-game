9038882f39709d2ce9bfce0c1e938b59d055ee47
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-09-06 19:01:03 -0400

    preventing app from sleep

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var http = require("http");
+
+setInterval(function() {
+    http.get("http://crystalclickgame.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 app.use(express.static('public'));
   module.exports = function (app) {
